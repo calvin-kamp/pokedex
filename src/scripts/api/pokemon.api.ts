ab23f194a9a6721f7c1d2123fd2ae855a537e9b0
Calvin Kamp <calvin.kamp6696@gmail.com>
2025-12-26 23:37:10 +0100

    feat(api): updated api-functions

--- src/scripts/api/pokemon.api.ts
+++ src/scripts/api/pokemon.api.ts
@@ -3,9 +3,15 @@ import type { ApiListResponse } from '@scripts/interfaces/api/list-response'
 import type { ApiPokemon } from '@scripts/interfaces/api/pokemon'
 
 export const fetchPokemonList = async (endpoint: string = 'pokemon'): Promise<ApiListResponse> => {
-    return fetchJson<ApiListResponse>(endpoint)
+    const baseUrl: string = import.meta.env.VITE_POKEAPI_BASE_URL
+    const apiUrl: string = endpoint.startsWith('https') ? endpoint : `${baseUrl}/${endpoint}`
+
+    return fetchJson<ApiListResponse>(apiUrl)
 }
 
 export const fetchPokemon = async (endpoint: string): Promise<ApiPokemon> => {
-    return fetchJson<ApiPokemon>(endpoint)
+    const baseUrl: string = import.meta.env.VITE_POKEAPI_BASE_URL
+    const apiUrl: string = endpoint.startsWith('https') ? endpoint : `${baseUrl}/${endpoint}`
+
+    return fetchJson<ApiPokemon>(apiUrl)
 }
