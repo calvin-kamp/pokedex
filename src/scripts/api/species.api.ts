6952d26a34132ccea59c688f22b05077ad7d4e7f
Calvin Kamp <calvin.kamp6696@gmail.com>
2025-12-26 06:22:58 +0100

    feat(api): updated species.api

--- src/scripts/api/species.api.ts
+++ src/scripts/api/species.api.ts
@@ -1,6 +1,9 @@
 import { fetchJson } from '@scripts/api/fetch-json'
 import type { ApiSpecies } from '@scripts/interfaces/api/species'
 
-export const fetchSpecies = async (endpointOrUrl: string): Promise<ApiSpecies> => {
-    return fetchJson<ApiSpecies>(endpointOrUrl)
+export const fetchSpecies = async (pokemonId: number): Promise<ApiSpecies> => {
+    const baseUrl: string = import.meta.env.VITE_POKEAPI_BASE_URL
+    const apiUrl: string = `${baseUrl}/pokemon-species/${pokemonId}`
+
+    return fetchJson<ApiSpecies>(apiUrl)
 }
