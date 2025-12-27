60de0dd95d06385cde3f0d7c7372eb96048e3e6e
Calvin Kamp <calvin.kamp6696@gmail.com>
2025-12-27 02:35:51 +0100

    chore(config): updated viteconfig and tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '@root': fileURLToPath(new URL('./src', import.meta.url)),
+            '@json': fileURLToPath(new URL('./src/json', import.meta.url)),
             '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
             '@scripts': fileURLToPath(new URL('./src/scripts', import.meta.url)),
         },
