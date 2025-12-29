48fe969f2a4e8a590cdb348aebaa43ff2ab3ca97
Calvin Kamp <calvin.kamp6696@gmail.com>
2025-12-29 19:24:29 +0100

    chore(config): - removed unusded script - initialized webapp - removed counter.ts

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,13 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { languageSwitch } from './scripts/components/language-switch'
+import { pokedex } from './scripts/components/pokedex'
+import { search } from './scripts/components/search'
+import { pokemonDialog } from './scripts/components/pokemon-dialog'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const init = () => {
+    languageSwitch.init()
+    pokedex.init()
+    search.init()
+    pokemonDialog.init()
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+init()
